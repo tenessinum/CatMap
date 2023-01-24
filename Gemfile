e3af8062f57811f667192bcee2dd0072693e9874
Igor Nurullaev <mighty.igor@yandex.ru>
2023-01-24 05:58:29 +0300

    Добавил еще тестов

--- Gemfile
+++ Gemfile
@@ -50,6 +50,7 @@ gem "bootsnap", require: false
 
 gem "devise"
 gem "faker"
+gem "rspec"
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- test/controllers/point_controller_test.rb
+++ test/controllers/point_controller_test.rb
@@ -2,12 +2,14 @@ require "test_helper"
 
 class PointControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @password = Faker::Lorem.word
+    @password = Faker::Lorem.word + Faker::Lorem.word
     @user = User.create(email: Faker::Internet.email, password: @password)
   end
 
   test "should not get create" do
-    post point_create_path
+    assert_no_changes('Point.count') do
+      post point_create_path, params: { title: Faker::Lorem.word, description: Faker::Lorem.word, lon: Faker::Address.longitude, lat: Faker::Address.latitude }
+    end
     assert_redirected_to new_user_session_path
   end
 
